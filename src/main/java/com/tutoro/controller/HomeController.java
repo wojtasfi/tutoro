e094ed52f60e0e41eb17c5da2e3f108bf7c961dd
Wojciech Figas <wojciech.figas@outlook.com>
2017-05-03 18:34:04 +0200

    Add view for all tutors

--- src/main/java/com/tutoro/controller/HomeController.java
+++ src/main/java/com/tutoro/controller/HomeController.java
@@ -36,7 +36,7 @@ public class HomeController {
         if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
-        return "redirect:/login?logout";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
+        return "redirect:/login?logout";
     }
 
 
--- src/main/java/com/tutoro/controller/TutorsController.java
+++ src/main/java/com/tutoro/controller/TutorsController.java
@@ -1,5 +1,6 @@
 package com.tutoro.controller;
 
+import com.tutoro.entities.Skill;
 import com.tutoro.entities.Tutor;
 import com.tutoro.service.TutorService;
 import org.slf4j.Logger;
@@ -10,14 +11,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by wojci on 4/26/2017.
  */
 @Controller
-@RequestMapping("/tutor")
+@RequestMapping("/")
 public class TutorsController {
 
     @Autowired
@@ -25,14 +29,30 @@ public class TutorsController {
 
     private static Logger LOGGER = LoggerFactory.getLogger(TutorsController.class);
 
+    @RequestMapping(value = "/tutors", method = POST)
+    public String findAllTutorstAfterLogin(Model model) {
+        return "redirect:/tutors";
+    }
 
     @RequestMapping(value = "/tutors", method = GET)
     public String findAllTutors(Model model) {
 
         List<Tutor> tutors = tutorService.findAll();
+
+        for (Tutor tutor : tutors) {
+            Set<Skill> skills = tutor.getSkills();
+
+            Set limitedSkills = (Set) skills
+                    .stream()
+                    .limit(3)
+                    .collect(Collectors.toSet());
+
+            tutor.setSkills(limitedSkills);
+
+        }
         model.addAttribute("tutors", tutors);
 
-        return "registration";
+        return "tutors";
     }
 
 }
